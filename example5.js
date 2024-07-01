f9ddba6f3e5917c2bfd581db1569ee51ddfb4bbe
M.ngugi <mngugi7@gmail.com>
2024-07-02 00:26:22 +0300

    Create example5.js

---
+++ example5.js
@@ -0,0 +1,12 @@
+var CardinalDirections;
+(function (CardinalDirections) {
+    CardinalDirections["North"] = "North";
+    CardinalDirections["East"] = "East";
+    CardinalDirections["South"] = "South";
+    CardinalDirections["West"] = "West";
+})(CardinalDirections || (CardinalDirections = {}));
+;
+// logs "North"
+console.log(CardinalDirections.North);
+// logs "West"
+console.log(CardinalDirections.West);
