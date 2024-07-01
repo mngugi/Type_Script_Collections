3c779ac489f39ea752e12263d941d038d80f8af3
M.ngugi <mngugi7@gmail.com>
2024-07-01 21:22:40 +0300

    example4.js

--- example4.js
+++ example4.js
@@ -1,6 +1,12 @@
-var fruits = {
-    type: "banana",
-    model: "platoon",
-    size: 12
-};
+var fruits = [{
+        type: "banana",
+        model: "platoon",
+        size: 12,
+    },
+    {
+        type: "apples",
+        model: "wanjohi",
+        size: 10
+    }
+];
 console.log(fruits);
--- example4.ts
+++ example4.ts
@@ -1,9 +1,15 @@
-const fruits: {type: string, model: string, size: number} = {
+const fruits: {type: string, model: string, size: number}[] =[ {
     type: "banana",
     model: "platoon",
-    size: 12
+    size: 12,
 
 
-};
+},
 
+{
+    type: "apples",
+    model:"wanjohi",
+    size: 10
+}
+]
 console.log(fruits);
