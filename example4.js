8b1e14d0daa6b46482ccce2c06a189c280aea1fe
M.ngugi <mngugi7@gmail.com>
2024-07-01 12:15:01 +0300

    example4.js

---
+++ example4.js
@@ -0,0 +1,6 @@
+var fruits = {
+    type: "banana",
+    model: "platoon",
+    size: 12
+};
+console.log(fruits);
---
+++ example4.ts
@@ -0,0 +1,9 @@
+const fruits: {type: string, model: string, size: number} = {
+    type: "banana",
+    model: "platoon",
+    size: 12
+
+
+};
+
+console.log(fruits);
