cfe2b17fa571b78431fff0e849095b3b60dde6d8
M.ngugi <mngugi7@gmail.com>
2024-07-02 00:25:08 +0300

    Create example5.ts

---
+++ example5.ts
@@ -0,0 +1,12 @@
+enum CardinalDirections {
+  North = 'North',
+  East = "East",
+  South = "South",
+  West = "West"
+};
+
+// logs "North"
+console.log(CardinalDirections.North);
+
+// logs "West"
+console.log(CardinalDirections.West);
